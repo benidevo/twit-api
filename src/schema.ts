97af6f6e0a34d3138e407b26cbb4f790466054b3
Benjamin Idewor <benjaminidewor@gmail.com>
2022-01-03 12:19:19 +0100

    feat: add postDelete mutation

--- src/schema.ts
+++ src/schema.ts
@@ -1,49 +1,50 @@
 import { gql } from 'apollo-server';
 
 export const typeDefs = gql`
-  type Query {
-    posts: [Post!]!
-  }
-  type Mutation {
-    postCreate(post: PostInputType!): PostOutputType!
-    postUpdate(id: String!, post: PostInputType!): PostOutputType!
-  }
+    type Query {
+        posts: [Post!]!
+    }
+    type Mutation {
+        postCreate(post: PostInputType!): PostOutputType!
+        postUpdate(id: String!, post: PostInputType!): PostOutputType!
+		postDelete(id: String!): PostOutputType!
+    }
 
-  type Error {
-    message: String
-  }
+    type Error {
+        message: String
+    }
 
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-    posts: [Post!]!
-    profile: Profile!
-    createdAt: String!
-  }
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        posts: [Post!]!
+        profile: Profile!
+        createdAt: String!
+    }
 
-  type Post {
-    id: ID!
-    title: String!
-    content: String!
-    published: Boolean!
-    author: User!
-    createdAt: String!
-  }
+    type Post {
+        id: ID!
+        title: String!
+        content: String!
+        published: Boolean!
+        author: User!
+        createdAt: String!
+    }
 
-  type PostOutputType {
-    errors: [Error!]!
-    post: Post
-  }
+    type PostOutputType {
+        errors: [Error!]!
+        post: Post
+    }
 
-  type Profile {
-    id: ID!
-    bio: String!
-    user: User!
-  }
+    type Profile {
+        id: ID!
+        bio: String!
+        user: User!
+    }
 
-  input PostInputType {
-    title: String
-    content: String
-  }
+    input PostInputType {
+        title: String
+        content: String
+    }
 `;
